24a429135771cb8c002b396e9fe45c4f2e1b060d
Chad Lindsay <chadlindsay01@gmail.com>
2016-01-05 13:57:59 -0400

    saved changes

--- SuperCool/ViewController.swift
+++ SuperCool/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CoolLogo: UIImageView!
+    @IBOutlet weak var CoolBG: UIImageView!
+    @IBOutlet weak var UncoolButton: UIButton!
+    @IBOutlet weak var MrWhite: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func MakeMeWalterWhite(sender: AnyObject) {
+        CoolLogo.hidden = false
+        CoolBG.hidden = false
+        UncoolButton.hidden = true
+        MrWhite.hidden = true
+    }
 }
 
